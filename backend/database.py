898e2260ded09eb091cd2b7d6357dd375ffb9738
Fernando221290 <nandoayalamuso@gmail.com>
2026-01-17 13:39:27 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from .config import DATABASE_URL
+from config import DATABASE_URL
 
 
 # Crea el motor de conexión a PostgreSQL
